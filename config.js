75c348a44aef5f52f7c26e7a23409569116d781f
MichaelYuVanderbilt <apple@apples-MacBook-Pro-3.local>
2022-03-15 17:49:11 -0500

    figured out database stuff

---
+++ config.js
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyBo7Z9_lTljySJYVR-LzmjIRi9wnXa4qd0",
+    authDomain: "dsc-test-dd32c.firebaseapp.com",
+    databaseURL: "https://dsc-test-dd32c-default-rtdb.firebaseio.com/",
+    projectId: "dsc-test-dd32c",
+    storageBucket: "dsc-test-dd32c.appspot.com",
+    messagingSenderId: "464283554098",
+    appId: "1:464283554098:web:3d245d05bd5c64bb65072b",
+    measurementId: "G-ER7STWGLQ3"
+  }
+  
+  export default config;
