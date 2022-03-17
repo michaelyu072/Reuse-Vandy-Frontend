6d4b30aa7ee5f6c732c31ad244e9e33bf8c3a248
“OliviaLiu” <xiaohan.liu.1@vanderbilt.edu>
2022-03-16 22:10:14 -0500

    upload form skeleton

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,27 +1,34 @@
 import React from "react";
+import Form from './UploadForm';
 import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
 import SearchIcon from '@mui/icons-material/Search';
 
 function SearchBar(props) {
+  const [buttonPopup, setButtonPopup] = React.useState(false);
 
   return (
     <section className = 'searchBarContainer'>
-        <div className = 'logoContainer'>   
+        <div className = 'logoContainer'>
             <p className = 'reuseWord'>Reuse</p>
             <p className = 'vandyWord'>Vandy</p>
         </div>
         <div className = 'searchBarBox'>
         <input placeholder = 'Search Reuse Vandy'
-         defaultValue = {props.keyword} 
-        className = 'searchBar' 
+         defaultValue = {props.keyword}
+        className = 'searchBar'
         // onChange = {(e) => {props.updateTerm(e.target.value);}}
         />
         <button className = 'searchButton'><SearchIcon/></button>
         </div>
         <div className = 'profileContainer'>
-        <button className = 'uploadButton'>
-            Upload Item
+        <button className = 'uploadButton'
+          onClick={()=> setButtonPopup(true)}>
+          Upload Item
         </button>
+        <Form
+          trigger = {buttonPopup}
+          setTrigger = {setButtonPopup}>
+          </Form>
             <AccountCircleRoundedIcon/>
         </div>
     </section>
