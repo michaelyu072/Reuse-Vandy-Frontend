a7e901ff7ce81d1b8ba87fe6fc07fb0108580592
MichaelYuVanderbilt <apple@apples-MacBook-Pro-3.local>
2022-03-26 23:54:39 -0500

    added placeholder photo

--- src/components/ResultItem.js
+++ src/components/ResultItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import CloseIcon from '@mui/icons-material/Close';
+import myImage from '../placeholder.png';
 
 function ResultItem(props) {
 
@@ -10,7 +11,7 @@ function ResultItem(props) {
       <div className = 'resultShadowBox'>
     <div className = 'resultItemBox'>
         <div className = 'itemImgBox'>
-        <img className = 'itemImg' src = {!obj.sellerPhoto.includes('undefined') ? obj.sellerPhoto : 'https://media.istockphoto.com/photos/isolated-shot-of-opened-blank-cardboard-box-on-white-background-picture-id520619396?k=20&m=520619396&s=612x612&w=0&h=4f0ZqIMkVHEIhq76j8qmI-nc55cBhx-nqgITFtoW6WI='}>
+        <img className = 'itemImg' src = {!obj.sellerPhoto.includes('undefined') ? obj.sellerPhoto : myImage}>
         </img>
         </div>
         <div className = 'itemCheckoutBox'>
